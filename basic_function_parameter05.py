d98b69e5753f60e5d3cc1e58de11df682868dc92
unknown <shamsiyevalibek90@gmail.com>
2025-04-30 14:28:43 +0300

    slina

--- basic_function_parameter05.py
+++ basic_function_parameter05.py
@@ -1,3 +1,12 @@
 # Create a function named count_vowels that takes a string as a parameter.
 # Inside the function, count the number of vowels (a, e, i, o, u, A, E, I, O, U) in the given string.
 # Return the count of vowels.
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+a=str(input())
+print(count_vowels(a))
